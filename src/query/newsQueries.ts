316bcb30525e32f642df8046bf606e6cc2590454
gogul-s <65705377+Gogul-S@users.noreply.github.com>
2022-12-09 02:01:23 +0530

    Update newsQueries.ts

--- src/query/newsQueries.ts
+++ src/query/newsQueries.ts
@@ -6,6 +6,17 @@ const KEYS = {
   NEWS_LIST_QUERY: 'NEWS_LIST_QUERY',
 };
 
+/**
+ *
+ * useInfiniteQuery was causing some issues when used in conjuction with persist library,
+ * hence not used for pagination.
+ *
+ * Instead a custom logic has been put into place by manipulating query keys.
+ * Ideally we should use refetch to fetch data with new param or use useInfiniteQuery for pagination, and avoid query re-rendering.
+ * Took this decision to manipulate keys,
+ * as even in offline mode the cached data against this keys would be given to view
+ * and it was very late to switch to a different state management library.
+ */
 export const useFetchNewsQuery = (pageNumber: number, query: string) => {
   return useQuery<NewsEntity[], string>({
     queryKey: [KEYS.NEWS_LIST_QUERY, pageNumber],
